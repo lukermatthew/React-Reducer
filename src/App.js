438d3031590158fe1f9fb940de20f8ac35205de8
Mat <tan.matmercado@gmail.com>
2022-09-29 15:57:51 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 const ACTION = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement',
-  NEW_USER_INPUT: 'newUserInput',
-  TG_COLOR: 'tgColor'
-}
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  NEW_USER_INPUT: "newUserInput",
+  TG_COLOR: "tgColor",
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -20,32 +20,39 @@ const reducer = (state, action) => {
     default:
       throw new Error();
   }
-}
+};
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0, userInput: '', color: false })
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    userInput: "",
+    color: false,
+  });
 
   return (
-    <main className="App" style={{ color: state.color ? '#FFF' : '#FFF952' }}>
+    <main className="App" style={{ color: state.color ? "#FFF" : "#FFF952" }}>
       <input
         type="text"
         value={state.userInput}
-        onChange={(e) => dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })}
+        onChange={(e) =>
+          dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })
+        }
       />
-      <br /><br />
+      <br />
+      <br />
       <p>{state.count}</p>
       <section>
-        <button onClick={(() => dispatch({ type: ACTION.DECREMENT }))}>-</button>
-        <button onClick={(() => dispatch({ type: ACTION.INCREMENT }))}>+</button>
-        <button onClick={(() => dispatch({ type: ACTION.TG_COLOR }))}>Color</button>
+        <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>-</button>
+        <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>+</button>
+        <button onClick={() => dispatch({ type: ACTION.TG_COLOR })}>
+          Color
+        </button>
       </section>
-      <br /><br />
+      <br />
+      <br />
       <p>{state.userInput}</p>
     </main>
   );
 }
 
 export default App;
-
-
-
